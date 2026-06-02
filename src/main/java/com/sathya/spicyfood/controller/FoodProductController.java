08e28b2a7c09c1730637d74aa3c8573e342c49ca
sivakumar-1919 <sivakumards419@gmail.com>
2026-06-02 11:06:30 +0530

    cors problem solved

--- src/main/java/com/sathya/spicyfood/controller/FoodProductController.java
+++ src/main/java/com/sathya/spicyfood/controller/FoodProductController.java
@@ -19,7 +19,7 @@ import com.sathya.spicyfood.service.FoodProductService;
 
 @RestController
 @RequestMapping("/products")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = { "http://localhost:5173","https://react-spicy-food.vercel.app/"} )
 public class FoodProductController {
      
 	@Autowired
